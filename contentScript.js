c485cdb019a703a4bc43c406af804dc2d48fff06
Matouš Ejem <ejem@mail.muni.cz>
2019-04-17 01:01:21 +0200

    cleaning

--- contentScript.js
+++ contentScript.js
@@ -44,34 +44,34 @@ $("textarea").each( function (index, currentTextarea) {
 //   });
 
 //
-// function tokenize(fakeDiv, hash, text) {
-//   $.ajax({
-//    type: 'POST',
-//    dataType: 'json',
-//    url: "https://nlp.fi.muni.cz/projekty/corrector/backend/tokenizer/tokenizer.cgi",
-//    data: {
-//        'hash': hash,
-//        'text': text
-//    },
-//    success: function (tokenizedData) {
-//        console.log(tokenizedData);
-//        var tokenized = [];
-//        for (var tokenArray of tokenizedData.tokens) {
-//          var token = document.createElement("g");
-//          $(token).attr("data-pk-token-type", tokenArray[0]);
-//          $(token).text(tokenArray[1]);
-//          tokenized.push(token);
-//        }
-//
-//        var text = datafy($(fakeDiv).html());
-//        var hash = hex_md5(text);
-//        if (hash == tokenizedData.hash) {
-//          $(fakeDiv).html("");
-//          $(fakeDiv).append(tokenized);
-//        }
-//    }
-//   });
-// };
+function tokenize(fakeDiv, hash, text) {
+  $.ajax({
+   type: 'POST',
+   dataType: 'json',
+   url: "https://nlp.fi.muni.cz/projekty/corrector/backend/tokenizer/tokenizer.cgi",
+   data: {
+       'hash': hash,
+       'text': text
+   },
+   success: function (tokenizedData) {
+       console.log(tokenizedData);
+       var tokenized = [];
+       for (var tokenArray of tokenizedData.tokens) {
+         var token = document.createElement("g");
+         $(token).attr("data-pk-token-type", tokenArray[0]);
+         $(token).text(tokenArray[1]);
+         tokenized.push(token);
+       }
+
+       var text = datafy($(fakeDiv).html());
+       var hash = hex_md5(text);
+       if (hash == tokenizedData.hash) {
+         $(fakeDiv).html("");
+         $(fakeDiv).append(tokenized);
+       }
+   }
+  });
+};
 
 // function htmlfy(dataText) {
 //   return dataText.replace(/&/g, '&amp;') //       & -> &amp;
