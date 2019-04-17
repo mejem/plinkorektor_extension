209b965010bc3ad177404a2d3ce03825fc0d36c7
Matouš Ejem <ejem@mail.muni.cz>
2019-04-18 00:36:53 +0200

    using hash as id was stupid

--- demopage/hl.js
+++ demopage/hl.js
@@ -11,23 +11,91 @@ $(".hwt-container").prepend("<div class='hwt-backdrop'></div>");
 $(".hwt-backdrop").append("<div class='hwt-highlights hwt-content'></div>");
 
 $("textarea").on("input", function () {
-  var marked = "";
-  var count = 0;
-  for (var w of $(this).val().split(" ")) {
-    if (!(count % 5)) {
-      marked += "<mark>" + w + "</mark> ";
+  // var $textarea = $(this);
+
+  var $highlights = $(this).parent().find(".hwt-highlights");
+  var lineSpans = [];
+  var lines = $(this).val().split("\n");
+  for (let line of lines) {
+    var hash = hex_md5(line);
+    let $spanId = $("#" + hash);
+    if ($spanId.length) {
+      console.log($spanId.text().length);
+      // console.log(hash, "exists");
+      lineSpans.push($spanId);
     } else {
-      marked += w + " ";
+      // console.log(hash, "changed");
+      var lineSpan = document.createElement("span");
+      // $(lineSpan).prop("id", hash).text(line).addClass("pk-line");
+      $(lineSpan).prop("id", hash).html("<mark>" + line + "\n" + "</mark>").addClass("pk-line");
+      lineSpans.push(lineSpan);
     }
-    count++;
   }
+  $highlights.empty();
+  $highlights.append(lineSpans);
+
+
+  // console.log(lineSpans);
+
+  // var marked = "";
+  // var count = 0;
+  // for (var w of $(this).val().split(" ")) {
+  //   if (!(count % 5)) {
+  //     marked += "<mark>" + w + "</mark> ";
+  //   } else {
+  //     marked += w + " ";
+  //   }
+  //   count++;
+  // }
   // console.log(marked);
-  $(this).parent().find(".hwt-highlights").html(marked);
-  $("mark").on("click", function () {
-    console.log($(this).text());
-    // $(this).append("<div class='underline'></div>");
+  // $(this).parent().find(".hwt-highlights").html(marked);
+  $($highlights).on("mouseenter", function () {
+    // console.log($(this).text());
+    $highlights.css("zIndex", 0);
+    setTimeout(function () {
+      $highlights.css("zIndex", 10);
+    }, 1000);
+    // $(this).prepend("<div class='pk-tooltip'>I am THE tooltip</div>");
   });
+  $("mark").on("mouseenter", function () {
+    // console.log($(this).text());
+    // $highlights.css("zIndex", 0);
+    // setTimeout(function () {
+    //   $highlights.css("zIndex", 10);
+    // }, 1000);
+    // $(this).prepend("<div class='pk-tooltip'>I am THE tooltip</div>");
+  });
+  // $("mark").on("mouseleave", function () {
+  //   console.log($(this).text());
+  //   // $textarea.css( "zIndex", 20 );
+  //
+  //   setTimeout(function () {
+  //     $highlights.css("zIndex", 10);
+  //   }, 1000);
+  //   // $(this).append("<div class='underline'></div>");
+  // });
+  // $("mark").on("hover", function () {
+  //   $highlights.zIndex(-10);
+  //   console.log($(this).text());
+  //   // $(this).append("<div class='underline'></div>");
+  // });
 });
+
+// $("mark").on("mouseover", function () {
+//   console.log($(this).text());
+//   // console.log($(this).closest(".hwt-highlights"));
+//   // $textarea.css( "zIndex", 20 );
+//   // $highlights.css("zIndex", 0);
+//   // $(this).append("<div class='underline'></div>");
+// });
+// $("mark").on("mouseleave", function () {
+//   console.log($(this).text());
+//   // console.log($(this).closest(".hwt-highlights"));
+//   // $textarea.css( "zIndex", 20 );
+//   // $highlights.css("zIndex", 0);
+//   // $(this).append("<div class='underline'></div>");
+// });
+
 
 $("textarea").on("scroll", function () {
   let scrollTop = $(this).scrollTop();
@@ -42,3 +110,32 @@ $(".hwt-backdrop").on("scroll", function () {
     backdrop.parent().find("textarea").scrollTop(scrollTop);
   }, 50));
 });
+
+function tokenize(fakeDiv, hash, text) {
+  $.ajax({
+   type: 'POST',
+   dataType: 'json',
+   url: "https://nlp.fi.muni.cz/projekty/corrector/backend/tokenizer/tokenizer.cgi",
+   data: {
+       'hash': hash,
+       'text': text
+   },
+   success: function (tokenizedData) {
+       console.log(tokenizedData);
+       var tokenized = [];
+       for (var tokenArray of tokenizedData.tokens) {
+         var token = document.createElement("g");
+         $(token).attr("data-pk-token-type", tokenArray[0]);
+         $(token).text(tokenArray[1]);
+         tokenized.push(token);
+       }
+
+       var text = datafy($(fakeDiv).html());
+       var hash = hex_md5(text);
+       if (hash == tokenizedData.hash) {
+         $(fakeDiv).html("");
+         $(fakeDiv).append(tokenized);
+       }
+   }
+  });
+};
